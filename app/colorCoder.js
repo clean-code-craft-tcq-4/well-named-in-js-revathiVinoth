44838558928e22e4ff89352ee0061a294bf5e005
Revathi Shanmugaboopathi <EHR1COB@rbeigcn.com>
2022-06-30 11:20:04 +0530

    New Feature

--- app/colorCoder.js
+++ app/colorCoder.js
@@ -28,11 +28,20 @@ function convertColortoNumber(majorColor, minorColor, expectedPairNumber) {
   console.assert(actualPairNumber == expectedPairNumber);
 }
 
+//New feature
+function testColorMap() {
+  for (var pairNumber = 1; pairNumber <= 25; pairNumber++) {
+    let testPair = getColorFromPairNumber(pairNumber);
+    console.log(`Pair Number: ${pairNumber},Colors:${testPair}`);
+    console.assert(pairNumber == getPairNumberFromColor(testPair));
+  }
+}
 function test() {
   convertNumbertoColor(4, "WHITE", "BROWN");
   convertNumbertoColor(5, "WHITE", "SLATE");
   convertNumbertoColor(23, "VIOLET", "GREEN");
   convertColortoNumber("YELLOW", "GREEN", 18);
   convertColortoNumber("RED", "BLUE", 6);
+  testColorMap();
 }
 test();
